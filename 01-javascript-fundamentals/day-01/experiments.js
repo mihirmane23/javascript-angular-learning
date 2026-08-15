8a7d2c4f9f80820d795388943c932e68ae73c189
mihirmane23 <mihirmane120@gmail.com>
2026-08-15 21:05:22 +0530

    feat: add day 1 javascript fundamentals exercises

---
+++ 01-javascript-fundamentals/day-01/experiments.js
@@ -0,0 +1,15 @@
+console.log("Hello, JavaScript!");
+console.log("I am learning JavaScript.");
+console.log(10);
+console.log(5 + 5);
+
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log("10" + "5");
+
+console.log("My name is Mihir");
+console.log(25);
+console.log(25 + 5);
+console.log("25" + 5);
