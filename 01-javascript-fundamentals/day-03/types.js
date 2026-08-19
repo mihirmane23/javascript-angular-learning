9847231d4cc204fd3291595ff909bb40b97db661
mihirmane23 <mihirmane120@gmail.com>
2026-08-19 20:17:50 +0530

    Merge pull request #1 from mihirmane23/feature/mihir/day-3
    Feature/mihir/day 3

---
+++ 01-javascript-fundamentals/day-03/types.js
@@ -0,0 +1,17 @@
+const score = 95;
+const username = "Mihir";
+const isLoggedIn = false;
+
+console.log(typeof score);
+console.log(typeof username);
+console.log(typeof isLoggedIn);
+
+let result = undefined;
+console.log(result);
+console.log(typeof result);
+
+console.log(undefined);
+
+let user = null;
+console.log(user);
+console.log(typeof user);
