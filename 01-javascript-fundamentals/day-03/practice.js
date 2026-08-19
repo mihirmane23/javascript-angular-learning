b261a3c5814a358fe5d9e50d750886d0304e4d43
mihirmane23 <mihirmane120@gmail.com>
2026-08-19 20:15:12 +0530

    feat: add day 3 javascript data types exercises

---
+++ 01-javascript-fundamentals/day-03/practice.js
@@ -0,0 +1,11 @@
+const name = "Mihir";
+let age = 24;
+const isLearningJavascript = true;
+const tomorrow = undefined;
+const error = null;
+
+console.log(`Name: ${name} | Type: ${typeof name}`);
+console.log(`Age: ${age} | Type: ${typeof age}`);
+console.log(`Is learning JavaScript: ${isLearningJavascript} | Type: ${typeof isLearningJavascript}`);
+console.log(`Tomorrow: ${tomorrow} | Type: ${typeof tomorrow}`);
+console.log(`Error: ${error} | Type: ${typeof error}`);
